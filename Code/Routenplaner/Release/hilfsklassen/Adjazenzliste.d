3c2f2d8fb8c828b28da90680fdb4a1b7eb5e233b
Christoph Drost <christoph.drost@htw-saarland.de>
2014-05-12 17:48:36 +0200

    kante geschickt

---
+++ Code/Routenplaner/Release/hilfsklassen/Adjazenzliste.d
@@ -0,0 +1,4 @@
+hilfsklassen/Adjazenzliste.d: ../hilfsklassen/Adjazenzliste.cpp \
+  ../hilfsklassen/Adjazenzliste.h
+
+../hilfsklassen/Adjazenzliste.h:
---
+++ Code/Routenplaner/hilfsklassen/Adjazenzliste.h
@@ -0,0 +1,19 @@
+/*
+ * Adjazenzliste.h
+ *
+ *  Created on: 12.05.2014
+ *      Author: christoph
+ */
+
+#ifndef ADJAZENZLISTE_H_
+#define ADJAZENZLISTE_H_
+
+class Adjazenzliste {
+public:
+	Adjazenzliste();
+	virtual ~Adjazenzliste();
+private:
+
+};
+
+#endif /* ADJAZENZLISTE_H_ */
